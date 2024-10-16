f5a8736a766cd84460e3abde4d4ac9eaf53a6a20
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-10-16 23:55:44 +0600

    added new style

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,17 +1,32 @@
-import React from "react";
+// import React from "react";
 import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
   UploadOutlined,
   UserOutlined,
   VideoCameraOutlined,
 } from "@ant-design/icons";
-import { Layout, Menu, theme } from "antd";
+import { Button, Layout, Menu, theme } from "antd";
+import { createElement, useState } from "react";
+
+const items = [
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+  UserOutlined,
+].map((icon, index) => ({
+  key: String(index + 1),
+  icon: createElement(icon),
+  label: `nav ${index + 1}`,
+}));
 
 const { Header, Content, Footer, Sider } = Layout;
 
 const MainLayout = () => {
+  const [collapsed, setCollapsed] = useState(false);
   return (
-    <Layout>
-      <Sider
+    <Layout style={{ height: "100vh" }}>
+      {/* <Sider
         breakpoint="lg"
         collapsedWidth="0"
         onBreakpoint={(broken) => {
@@ -28,19 +43,54 @@ const MainLayout = () => {
           defaultSelectedKeys={["4"]}
           items={items}
         />
+      </Sider> */}
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["1"]}
+          items={[
+            {
+              key: "1",
+              icon: <UserOutlined />,
+              label: "nav 1",
+            },
+            {
+              key: "2",
+              icon: <VideoCameraOutlined />,
+              label: "nav 2",
+            },
+            {
+              key: "3",
+              icon: <UploadOutlined />,
+              label: "nav 3",
+            },
+          ]}
+        />
       </Sider>
       <Layout>
-        <Header style={{ padding: 0, background: colorBgContainer }} />
+        <Header style={{ padding: 0 }}>
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: "20px",
+              width: 32,
+              height: 32,
+              color: "white",
+            }}
+          ></Button>
+        </Header>
         <Content style={{ margin: "24px 16px 0" }}>
           <div
             style={{
               padding: 24,
               minHeight: 360,
-              background: colorBgContainer,
-              borderRadius: borderRadiusLG,
             }}
           >
-            content
+            <h1>The main content should go here</h1>
           </div>
         </Content>
         <Footer style={{ textAlign: "center" }}>
